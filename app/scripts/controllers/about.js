620e855e83101c56c8efa4fd20a6d3433bb5c811
Peter Vargovcik <peter.vargovcik@ark-energy.eu>
2016-01-18 15:06:34 +0000

    Initial Comit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name arkAutoBuildTestRepoApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the arkAutoBuildTestRepoApp
+ */
+angular.module('arkAutoBuildTestRepoApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name arkAutoBuildTestRepoApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the arkAutoBuildTestRepoApp
+ */
+angular.module('arkAutoBuildTestRepoApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
